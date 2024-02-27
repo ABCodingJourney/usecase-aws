b982509faff65b51022e687cd240e0829668d033
ABCodingJourney <ananyabhat1814@gmail.com>
2024-02-27 14:00:55 +0530

    changing file paths to ec2 instance

--- pkg/params.py
+++ pkg/params.py
@@ -1,26 +1,16 @@
-# Snowflake connection configs
-
-# user = "adminAB"
-# password = "AdminWelcome@1"
-# account_identifier = "lwujfwh-wr47734"
-# database = "SNOWFLAKETRAINING"
-# schema = "LANDING_ZONE"
-
 """File names and constant variables"""
 
-# File names for unzipping
+# # File names for unzipping
 
-filename = "C:\\Users\\ananya.bhat\\PythonPractice\\Pandas\\data\\Clickstream\\01-hgprod_20231217-160000.tsv.gz"
-targetfolder = (
-    "C:\\Users\\ananya.bhat\\PythonPractice\\Phase1_UseCase\\data\\clickstream-data.tsv"
-)
+# filename = "C:\\Users\\ananya.bhat\\PythonPractice\\Pandas\\data\\Clickstream\\01-hgprod_20231217-160000.tsv.gz"
+# targetfolder = (
+#     "C:\\Users\\ananya.bhat\\PythonPractice\\Phase1_UseCase\\data\\clickstream-data.tsv"
+# )
 
 # File names for mapping
 
-col_file = "C:/Users/ananya.bhat/PythonPractice/Phase1_UseCase/data/column_headers.tsv"
-data_file = (
-    "C:/Users/ananya.bhat/PythonPractice/Phase1_UseCase/data/clickstreamdata.tsv"
-)
+col_file = "/home/ec2-user/UsecasePhase1/data/column_headers.tsv"
+data_file = "/home/ec2-user/UsecasePhase1/data/clickstreamdata.tsv"
 
 # Json loading variables
 
